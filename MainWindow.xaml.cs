1e0144151e8c7d67447f83c28d6e3410cac7e279
Karcub <rokaszemumenyecske@gmail.com>
2021-06-28 13:24:06 +0200

    Wpf base

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
